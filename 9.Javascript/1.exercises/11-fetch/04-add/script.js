077a2b6fe47e16fcff5f65ecc1421c0a108188d7
Lena B <benoit.lena@protonmail.com>
2020-01-23 16:44:48 +0100

    fini fetch damn

--- 9.Javascript/1.exercises/11-fetch/04-add/script.js
+++ 9.Javascript/1.exercises/11-fetch/04-add/script.js
@@ -1,28 +1,38 @@
 
 (() => {
     document.getElementById("run").addEventListener("click", () =>{
-        /*  */
+        /* au click execute function */
         let recupName = document.getElementById("hero-name").value;
+        /* creer variable qui recup la val de l'id  */
         let recupAlt = document.getElementById("hero-alter-ego").value;
+        /* creer variable qui recup la val de l'id  */
         let recupPower = document.getElementById("hero-powers").value;
+        /* creer variable qui recup la val de l'id  */
 
         fetch("http://localhost:3000/heroes", {
+            /* va chercher sur (lechemin) */
 
             method: 'POST',
+            /* methode post = envoyé truc sur serveur */
             headers : {
                 'Content-Type':'application/json;charset=UTF-8'
+                /* met ça */
 
             },
             body: JSON.stringify({
+                /* body rend en string */
 
                 name: recupName,
+                /* propriété + val (nom variable) */
                 alterego: recupAlt,
                 power: recupPower,
             }),
 
         })
-        .then((res) => res.json())
+        .then((response) => response.json())
+        /* seulement qd c fait prend la réponse met la en json */
         .then((data) => console.log(data)) 
+        /* seulement  */
         
 
         
--- 9.Javascript/1.exercises/11-fetch/05-delete/script.js
+++ 9.Javascript/1.exercises/11-fetch/05-delete/script.js
@@ -1,10 +1,13 @@
 
+document.getElementById("run").addEventListener("click", () => {
 
-(() => {
-    // get element b id run add event click
-    // recup id ds formu :> cree variable
-    // supp le perso de l'id recup ds api
-    // console log result
-    // async? qd t'as reup l'id supprime le 
+    let dataInput = document.getElementById("hero-id").value; 
+    /* declarer variable qui prend la valeur de l'id mit */
+    ///console.log(dataInput);
     
-})();
+    fetch('http://localhost:3000/heroes/'+dataInput,{
+    method: 'DELETE',
+    /* methode qui delete */
+    })
+    
+})
