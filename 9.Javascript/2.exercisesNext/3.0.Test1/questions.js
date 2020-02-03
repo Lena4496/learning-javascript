2132437dbc9e89e8031b86a732e4b7bea49b2ced
Lena B <benoit.lena@protonmail.com>
2020-02-03 14:04:37 +0100

    jasmine fini

--- 9.Javascript/2.exercisesNext/3.0.Test1/questions.js
+++ 9.Javascript/2.exercisesNext/3.0.Test1/questions.js
@@ -12,34 +12,62 @@ let remplaceECar = (texte) => {
     return text;
 }
 let concatString = (texte1, texte2) => {
+    let be = texte1+texte2; // ou : concat() methode
+    return be 
 
 }
 let afficherCar5 =  (texte) => {
+    let str = texte.charAt(4)
+    return str
 
 }
 let afficher9Car =  (texte) => {
+    let str = texte.substr(0,9);
+    return str
 
 }
 let majusculeString =  (texte) => {
+    let maj = texte.toUpperCase();
+    return maj
 
 }
 let minusculeString =  (texte) => {
+    let min = texte.toLowerCase();
+    return min
 
 }
 let SupprEspaceString =  (texte) => {
+    let noSpace = texte.trim();
+    return noSpace
 
 }
 let IsString =  (texte) => {
+    if(typeof texte ==='string'){
+    return true
+    }
+    else{
+        return false
 
+    };
+
+  
 }
 
 let AfficherExtensionString =  (texte) => {
 
+    return texte.split(".").pop();
+
 }
 let NombreEspaceString =  (texte) => {
+  
+    return texte.split(" ").length - 1 ;
+    
 
 }
 let InverseString =  (texte) => {
+
+    return texte.split("").reverse().join('');
+    // split met en tableau les elements , reverse les inverses et join les remet ensemble
 
 }
 
@@ -47,20 +75,39 @@ let InverseString =  (texte) => {
  * Exercices sur les nombres et les caluls mathématiques
  */
 let calculPuissance =  (x, y) => {
+    return Math.pow(x,y)
+    // math pow calcule puissance d'un nombre = x nbr a mettre en puissance y = puissance a faire
 
 }
 let valeurAbsolue =  (nombre) => {
+    return Math.abs(nombre)
 
 }
 let valeurAbsolueArray =  (array) => {
+    for (let i = 0; i < array.length; i++) {
+        
+        array[i]= Math.abs(array[i])
+        
+    }
+    return array
+
 
 }
 let sufaceCercle =  (rayon) => {
 
+    let surface = Math.PI * (rayon * rayon);
+    return Math.round(surface)
 }
 let hypothenuse =  (ab, ac) => {
+    
+    let hypo = Math.hypot(ab, ac)
+    return hypo
 
 }
 let calculIMC =  (poids, taille) => {
 
+    let imc = poids /(taille * taille);
+    return Math.round(imc*100)/100
+
+    // 
 }
